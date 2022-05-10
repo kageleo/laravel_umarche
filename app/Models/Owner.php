c794699f4685916c565986c8d92ee4692701aaf7
kagereo <60954189+kagereo@users.noreply.github.com>
2022-05-11 03:30:10 +0900

    image, create, validation

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Shop;
+use App\Models\Image;
 
 class Owner extends Authenticatable
 {
@@ -42,7 +43,7 @@ class Owner extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function shop(){
-        return $this->hasOne(Shop::class);
+    public function image(){
+        return $this->hasMany(Image::class);
     }
 }
